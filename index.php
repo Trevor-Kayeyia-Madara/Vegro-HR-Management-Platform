d4fe2d7683060dda94fbca72be722283dd74c55e
Trevor-Kayeyia-Madara <trevormadarakayeyia@gmail.com>
2026-03-07 16:37:22 +0300

    Clean repo

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Include necessary configurations or start sessions here
-// For example, include 'config/config.php' if needed
-
-// Redirect to the login page
-header("Location: login.php");
-exit(); // Ensure that subsequent code does not execute after redirection
-?>
