dea983527753b92fbd19ef2327f5cdd134b88df7
Trevor-Kayeyia-Madara <trevormadarakayeyia@gmail.com>
2026-03-24 13:39:56 +0300

    Docker

--- vegro-hr/vegro-hr-frontend/src/services/api.js
+++ vegro-hr/vegro-hr-frontend/src/services/api.js
@@ -1,19 +1,23 @@
-import axios from "axios"; 
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_BASE_URL
+  ? `${import.meta.env.VITE_API_BASE_URL.replace(/\/$/, "")}/api`
+  : "http://localhost:8000/api";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api",
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-    },
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
-api.interceptors.request.use((config => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config; 
-}));
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
